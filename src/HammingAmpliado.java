c65779f45b251dacf54d2b179b241abfd762f484
Raymond S Calviño De Abreu <rcdeabreu@esei.uvigo.es>
2019-05-23 20:00:21 +0200

    Update HammingAmpliado.java
    mostrar las matrices

--- src/HammingAmpliado.java
+++ src/HammingAmpliado.java
@@ -2,13 +2,13 @@
 public class HammingAmpliado {
 	//codigo Hamming Ampliado. Ejemplo 3.1.4 apuntes Teoría de Códigos ESEI
 	//Creación de las matrices H*(H) H*tr(Htr) y G*(G)
-	int [][] H = {	{0,0,0,0,0,0,0,1,1,1,1,1,1,1,1,0},
+	private static int [][] H = {	{0,0,0,0,0,0,0,1,1,1,1,1,1,1,1,0},
 					{0,0,0,1,1,1,1,0,0,0,0,1,1,1,1,0},
 					{0,1,1,0,0,1,1,0,0,1,1,0,0,1,1,0},
 					{1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0},
 					{1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1}
 				 };
-	int [][] Htr = {{0,0,0,1,1},
+	private static int [][] Htr = {{0,0,0,1,1},
 					{0,0,1,0,1},
 					{0,0,1,1,1},
 					{0,1,0,0,1},
@@ -25,7 +25,7 @@ public class HammingAmpliado {
 					{1,1,1,1,1},
 					{0,0,0,0,1},
 				   };
-	int [][] G = {{0,0,1,1,1,0,0,0,0,0,0,0,0,0,0,1},
+	private static int [][] G = {{0,0,1,1,1,0,0,0,0,0,0,0,0,0,0,1},
 				  {1,0,0,1,1,0,0,0,0,0,0,0,0,0,0,1},
 				  {0,1,0,1,0,1,0,0,0,0,0,0,0,0,0,1},
 				  {1,1,0,1,0,0,1,0,0,0,0,0,0,0,0,0},
@@ -37,9 +37,49 @@ public class HammingAmpliado {
 				  {0,1,0,1,0,0,0,1,0,0,0,0,0,1,0,0},
 				  {1,1,0,1,0,0,0,1,0,0,0,0,0,0,1,1},
 				 };
+	public static int [][] getH(){
+		return H;
+	}
+	
+	public int [][] getHtr(){
+		return Htr;
+	}
+	public int [][] getG(){
+		return G;
+	}
+	public void mostrarH(){
+		for (int x=0; x < getH().length; x++) {
+			System.out.printf("|");  
+			for (int y=0; y < getH()[x].length; y++) {
+				  System.out.printf(" %d ",getH()[x][y]);	
+			  }
+			System.out.printf("|");
+			System.out.println();	
+		}
+	}
+	public void mostrarHtr(){
+		for (int x=0; x < getHtr().length; x++) {
+			System.out.printf("|");  
+			for (int y=0; y < getHtr()[x].length; y++) {
+				  System.out.printf(" %d ",getHtr()[x][y]);	
+			  }
+			System.out.printf("|");
+			System.out.println();	
+		}
+	}
+	public void mostrarG(){
+		for (int x=0; x < getG().length; x++) {
+			System.out.printf("|");  
+			for (int y=0; y < getG()[x].length; y++) {
+				  System.out.printf(" %d ",getG()[x][y]);	
+			  }
+			System.out.printf("|");
+			System.out.println();	
+		}
+	}
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		
 	}
 
 }
