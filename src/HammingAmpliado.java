cb882532ae7817a50bf803f8f7840bfcc2c04eb7
Raymond S Calviño De Abreu <rcdeabreu@esei.uvigo.es>
2019-05-24 16:24:28 +0200

    Update HammingAmpliado.java
    borrado funcion multiplicar que no iba
    corregido funcion calcular traspuesta, aunque no hacia falta
    adecentado el main

--- src/HammingAmpliado.java
+++ src/HammingAmpliado.java
@@ -3,6 +3,7 @@ public class HammingAmpliado {
 	//codigo Hamming Ampliado. Ejemplo 3.1.4 apuntes Teoría de Códigos ESEI
 	//Para m=4, H(4,2)  codigo (15,11)
 	//Creación de las matrices H*(H) H*tr(Htr) y G*(G)
+	//System.out.println("Matriz=> Filas:"+Matriz.length+" Columnas: "+ Matriz[0].length);
 	//H 5 Filas x 16 Columnas
 	private static int [][] H = {   {0,0,0,0,0,0,0,1,1,1,1,1,1,1,1,0},
 									{0,0,0,1,1,1,1,0,0,0,0,1,1,1,1,0},
@@ -46,34 +47,26 @@ public class HammingAmpliado {
 	public static int [][] getHtr(){ return Htr; }
 	public static int [][] getG(){ return G; }
 	
+	//no se usa
 	public static void mostrarMatriz(int[][] a) {
-		
 		for(int i=0;i<a.length;i++) { 
     		for(int j=0;j<a[0].length;j++) { 
     		System.out.print(a[i][j] + " "); 
     		} 
     		System.out.println(); 
     		} 
-		
 	}
 	
 	public static int[][] calcularIdentidad(int tam){
 		int [][] I = new int[tam][tam];
-		
-		for(int i=0;i<tam;i++) { 
-			
-    		for(int j=0;j<tam;j++) { 
-    			
-    		if(i==j) {I[i][j]=1;}
-    		
-    		else	{I[i][j]=0;}
-    		
-    		}
-    			
-    		}
-		
+			for(int i=0;i<tam;i++) { 
+	    		for(int j=0;j<tam;j++) { 
+		    		if(i==j) {I[i][j]=1;}
+		    			else {I[i][j]=0;}
+	    		}
+    		}	
 		return I;
-    		} 
+    } 
 	
 	public static int[][] multiplicarMatrices(int[][] a, int[][] b) {
 	    int[][] c = new int[a.length][b[0].length];
@@ -96,25 +89,24 @@ public class HammingAmpliado {
 		int[][] c = a;
 		for (int x=0; x < a.length; x++) {
 			  for (int y=0; y < a[x].length; y++) {
-				  c[x][y] = a[x][y]+b[x][y];
-				  
+				  c[x][y] = a[x][y]+b[x][y];	  
 				  if(c[x][y]==2) {c[x][y]=0;}
 			  }
-			}
+		}
 		return c;
-    		} 
+    } 
 	
 	
 	public static int[][] traspuesta(int[][] a){
-		int[][] tr = a;
+		int[][] tr = new int [a[0].length][a.length];
 		for (int x=0; x < a.length; x++) {
 			  for (int y=0; y < a[x].length; y++) {
 				  tr[y][x] = a[x][y];
 			  }
-			}
+		}
 		
 		return tr;
-    		} 
+    } 
 
 	public static int peso(int[][] matriz){
 		int weight = 12;
@@ -124,10 +116,11 @@ public class HammingAmpliado {
 				  if (matriz[x][y] == 1) {w++;};
 			  }
 			  if (w<weight) {weight=w;}
-			}
+		}
 		
 		return weight;
-    		}
+    }
+	
 	public static void mostrarMatriz(int matrizM [][], String mensaje ){
 		System.out.println(mensaje+"\n");
 		for (int x=0; x < matrizM.length; x++) {
@@ -140,33 +133,7 @@ public class HammingAmpliado {
 		}
 		System.out.println("--------------------------------------------------");
 	}
-//esta mal la multiplicacion, al ser en binario no se si tiene en cuenta el acarreo o no, creo que no
-	public static int[][] multiplicarMatriz(int matrizA[][], int matrizB[][]){
-	    if(matrizA[0].length!=matrizB.length){
-	         throw new RuntimeException("Dimensiones Incompatibles");
-	    }else{
-	         int [][] matrizC = new int[matrizA.length][matrizB[0].length];
-	         for(int i = 0;i < matrizA.length;i++ ){
-	           for(int j = 0;j < matrizB[0].length;j++){
-	             for(int k = 0;k <matrizA[0].length;k++) {
-	            	 if(matrizC[i][j] == 1 & matrizA[i][k] == 1) {
-	            		 matrizC[i][j] = 0 * matrizB[k][j];
-	            	 }
-	            	 if(matrizC[i][j] == 0 & matrizA[i][k] == 1) {
-	            		 matrizC[i][j] = 1 * matrizB[k][j];
-	            	 }
-	            	 if(matrizC[i][j] == 1 & matrizA[i][k] == 0) {
-	            		 matrizC[i][j] = 1 * matrizB[k][j];
-	            	 }
-	            	 if(matrizC[i][j] == 0 & matrizA[i][k] == 0) {
-	            		 matrizC[i][j] = 0 * matrizB[k][j];
-	            	 }
-	             }
-	           }
-	         }
-	      return matrizC;
-	    } 
-	}
+
 	public static int [][] getSindrome(int c [][]) {
 		/**El vector fuente u = 1011, que en el código H(3, 2) se codifica como c = 0110011, 
 		 * es codificado ahora como c* = 01100110. 
@@ -180,30 +147,30 @@ public class HammingAmpliado {
 		 * r* (H*)tr = 100|0, esto nos indica que se ha producido un error al menos doble que 
 		 * no es posible corregir.
 		 */
-		return multiplicarMatriz(c,getHtr());
+		return multiplicarMatrices(c,getHtr());
 	}
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		mostrarMatriz(getH(),"Matriz H*");
+		System.out.println("H=> Filas:"+getH().length+" Columnas: "+getH()[0].length);
+		
 		mostrarMatriz(getHtr(),"Matriz Htr*");
-		//traspuesta(getH());
+		mostrarMatriz(traspuesta(getH()),"traspuesta por funcion");
+		
 		mostrarMatriz(getG(),"Matriz G*");
-		System.out.println("G=> Columnas:"+getG()[0].length+" filas: "+getG().length);
+		System.out.println("G=> Filas:"+getG().length+" Columnas: "+getG()[0].length);
 		//palabra 1 Filas x 11 Columnas
 		//		int [][] palabra = {{0},{0},{0},{0},{0},{0},{1},{0},{0},{0},{0}};
 
 		int [][] palabra = {{0,0,0,0,0,0,1,0,0,0,0}};
 		mostrarMatriz(palabra,"Vector palabra");
-		System.out.println("Palabra=> Columnas:"+palabra[0].length+" filas: "+palabra.length);
+		System.out.println("Palabra=> Filas:"+palabra.length+" Columnas: "+palabra[0].length);
 		
-		System.out.println("G=> Columnas:"+getG()[0].length+" Palabra=> filas: "+palabra.length);
 		
 		//Codificar palabra
-		int [][] palabraCodficada = multiplicarMatriz(palabra,getG());
+		int [][] palabraCodficada = multiplicarMatrices(palabra,getG());
 		mostrarMatriz(palabraCodficada,"palabraCodficada: ");
-		System.out.println("palabraCodficada=> filas:"+palabraCodficada[0].length+" Columnas: "+palabraCodficada.length);
-		mostrarMatriz(multiplicarMatrices(palabra,getG()),"palabraCodficada: ");
-		
+		System.out.println("palabraCodficada=> filas:"+palabraCodficada.length+" Columnas: "+palabraCodficada[0].length);
 		/*
 		mostrarMatriz(getSindrome(palabraCodficada),"Sindrome palabraCodficada: ");
 		
